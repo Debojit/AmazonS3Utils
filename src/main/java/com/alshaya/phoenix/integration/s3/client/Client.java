0904862271e02ccc8af57e7a567aea90aee96a15
Debojit Sinha <debojit.sinha@oracle.com>
2019-05-06 10:28:58 +0300

    Added credentials profile selection option.

--- src/main/java/com/alshaya/phoenix/integration/s3/client/Client.java
+++ src/main/java/com/alshaya/phoenix/integration/s3/client/Client.java
@@ -2,6 +2,7 @@ package com.alshaya.phoenix.integration.s3.client;
 
 import com.alshaya.phoenix.integration.s3.exception.InvalidRegionException;
 
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -10,6 +11,18 @@ public class Client {
 		S3Client client = null;
 		if (Region.regions().contains(Region.of(regionName)))
 			client = S3Client.builder()
+							   .region(Region.of(regionName))
+							   .build();
+		else
+			throw new InvalidRegionException("Invalid region " + regionName + ". Process aborted.");
+		return client;
+	}
+	
+	public static S3Client createClient(String regionName, String credentialsProfile) throws InvalidRegionException{
+		S3Client client = null;
+		if (Region.regions().contains(Region.of(regionName)))
+			client = S3Client.builder()
+							   .credentialsProvider(ProfileCredentialsProvider.builder().profileName(credentialsProfile).build())
 							   .region(Region.of(regionName))
 							   .build();
 		else
